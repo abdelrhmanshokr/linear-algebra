765748ed62ae95f517152d42af4afe303c9217ed
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2020-04-11 11:01:59 +0200

    added all functions in the second class of linear algebra refresher course except for orhtognal check and orthognal check

--- first_file/vector.py
+++ first_file/vector.py
@@ -105,15 +105,62 @@ class Vector(object):
             return error 
 
     def is_orthognal(self, v):
+        orthognal = False
         if self.dot_product(v) == 0:
             orthognal = True
-        else:
-            orthognal = False
         
         return orthognal
 
-v1 = Vector([-7.579,-7.88])
-v2 = Vector([22.737, 23.64])
+    def projection(self, v_vector):
+        #this function returns the projection vector of the v_vector into the base vector self
+        #note that self is the base b vector and v_vector is the vector which is required to make projection into the base vector 
+        try:
+            base_vector_dot_v_vector = self.dot_product(v_vector)
+            mag_of_v_vector = base_vector_dot_v_vector / self.magnitude()
+            #dividing once more over mag of the base vector
+            mag_of_v_vector = mag_of_v_vector / self.magnitude()
+            v_parallel_result = self.scale_mul(mag_of_v_vector)
+        except ZeroDivisionError as error:
+            print(error)
+        
+        return v_parallel_result
+
+    def v_orthognal(self, v_vector):
+        #this function returns the orthognal vector as the v_vector is projected over the base vector self
+        #the general form is v_vector = v_parallel + v_orthognal
+        # v_parallel= self.projection(v_vector)
+        # v_orthognal = Vector.sub(v_vector, v_parallel)
+
+        # return v_orthognal
+        return 'there is an issue with this function'
+
+    def composing_a_vector_into_two_components(self, v_vector):
+        #sovlv the above method first 
+        return 'solve the above method first'
+    
+    def cross_product(self, v):
+        #this function returns the cross product result for two dimension or three dimension vectors
+        cross_product_result = []
+        cross_product_result.append((self.coordinates[1] * v.coordinates[2]) - (self.coordinates[2] * v.coordinates[1]))
+        cross_product_result.append((self.coordinates[2] * v.coordinates[0]) - (self.coordinates[0] * v.coordinates[2]))
+        cross_product_result.append((self.coordinates[0] * v.coordinates[1]) - (self.coordinates[1] * v.coordinates[0]))
+
+        return cross_product_result  
+
+    def area_of_parallelgram(self, v):
+        cross_product_result = self.cross_product(v)
+        magnitude_result = 0
+        
+        for i in range(len(cross_product_result)):
+            magnitude_result +=(math.pow(cross_product_result[i], 2))
+        
+        return math.sqrt(magnitude_result)    
+    
+    def area_of_triangle(self, v):
+        return (self.area_of_parallelgram(v) / 2)
+        
+v1 = Vector([8.462, 7.893, -8.187])
+v2 = Vector([6.984, -5.975, 4.778])
 
 print('addition', Vector.add(v1, v2))
 print('subtraction', Vector.sub(v1, v2))
@@ -124,3 +171,9 @@ print('dot poduct', Vector.dot_product(v1, v2))
 print('angel of dot product', Vector.angel(v1, v2))
 print('is it parallel', Vector.is_parallel(v1, v2))
 print('is it orthognal', Vector.is_orthognal(v1, v2))
+print('this is the projection result', Vector.projection(v1, v2))
+print('this is the orthognal vector on the base vector in projection', Vector.v_orthognal(v1, v2))
+# print('this is decomposing of a vector v into two vectors one orthognal and the other parallel to the base vector b', Vector.decomposing_vector_into_a_parallel_and_an_orthognal_to_the_base(v1, v2))
+print('this is the cross product result', Vector.cross_product(v1, v2))
+print('this is the area of the parallelgram in the cross product', Vector.area_of_parallelgram(v1, v2))
+print('this is the area of the triangle in the cross product', Vector.area_of_triangle(v1, v2))
